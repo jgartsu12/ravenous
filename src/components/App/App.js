9ec29ce14b00b6df22f5a7a1c99e29459d046d83
jgartsu12 <jgartsu12@gmail.com>
2020-03-08 13:00:43 -0400

    app component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from '../../logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
+import BusinessList from '../BusinessList/BusinessList';
+import SearchBar from '../SearchBar/SearchBar';
+
+
+export default class App extends Component {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ravenous</h1>
+        <SearchBar />
+        <BusinessList/>
     </div>
   );
-}
+};
 
-export default App;
+
