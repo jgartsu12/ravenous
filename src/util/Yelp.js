8ae2dfc820260d6b92138678f0415da2c4440111
jgartsu12 <jgartsu12@gmail.com>
2020-03-11 12:44:08 -0400

    created yelp util

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -6,5 +6,27 @@ const Yelp = {
 };
 
 const search = async (term, location, sortBy) => {
-        return await fetch()
-    }
+        return await fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+            headers: {
+                Authorization: `Bearer ${apiKey}`
+            }
+        }).then(response => {
+            return response.json();
+        }).then(jsonResponse => {
+            if(jsonResponse.businesses) {
+            return jsonResponse.businesses.map(business => {
+                id: business.id,
+                imageSrc: business.image_url,
+                name: business.name,
+                address: business.address,
+                city: business.city,
+                state: business.state,
+                zipCode: business.zipCode,
+                category: business.category,
+                rating: business.rating,
+                reviewCount: business.reviewCount
+            });
+            }
+        })
+
+export.modules.Yelp;
