a942ec1dc42cc556047722dccceaa298803c2167
jgartsu12 <jgartsu12@gmail.com>
2020-03-11 18:51:23 -0400

    debugged Business list component

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -9,12 +9,12 @@ class BusinessList extends React.Component {
             <div className="BusinessList">
                 {
                     this.props.businesses.map(business => {
-                        return <Business business={business} key={business.id}/>
+                        return <Business business={business} key={business.id} />
                     })
-                };
+                }
             </div>
         );
-    };
-};
+    }
+}
 
 export default BusinessList;
